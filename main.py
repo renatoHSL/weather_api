e72b3f241de6a2b1fced5401c6253949d8ae9b32
Renato <renato92.hora@gmail.com>
2024-07-15 14:20:32 -0300

    Exercise done part 1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/api/v1/<static>/<date>")
+@app.route("/api/v1/<station>/<date>")
 def about(station, date):
     temperature = 23
     return {"station": station,
